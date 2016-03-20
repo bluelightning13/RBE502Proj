a59da947e2809dd057c3dc130db7ee77f5f1d00c
bluelightning13 <mehiatt@wpi.edu>
2016-03-20 15:07:16 -0400

    accelThrustUpdate

--- outline.m
+++ outline.m
@@ -10,8 +10,15 @@ function main()
 
 
 	% function for linear acceleration
-	function accel = acceleration()
-
+	function accel = acceleration(cInput, angles, xd, m, k, kd)
+		grav = [0; 0; -9.8];
+		fd = -kd * xd;
+		rx = [1 0 0 ; 0 cos() -sin(); 0 sin() cos()];
+		ry = [cos() 0 sin(); 0 1 0; -sin() 0 cos()];
+		rz = [cos() -sin() 0; sin() cos() 0; 0 0 1];
+		R = [rx ; ry ; rz ];
+		th = R * thrust(cInput, k);
+		accel = grav + 1/m *th + fd;
 	end
 
 
@@ -22,8 +29,8 @@ function main()
 
 
 	% function to calculate thrust
-	function thf = thrust()
-
+	function thf = thrust(cInput, k)
+		thf = [0; 0; k * sum(cInput)];
 	end
 
 
