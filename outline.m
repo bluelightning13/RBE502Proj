b3949f44b9f407817e92b0b5d306acbe79bc4c86
bluelightning13 <mehiatt@wpi.edu>
2016-03-20 14:12:27 -0400

    outline
    outline for matlab folder

---
+++ outline.m
@@ -0,0 +1,43 @@
+% main function where everything runs
+function main()
+
+
+
+
+	% function to display
+	function [x] = display()
+
+	end 
+
+
+	% function for linear acceleration
+	function accel = acceleration()
+
+	end
+
+
+	% function for angular acceleration
+	function angaccel = ang_acceleration()
+
+	end
+
+
+	% function to calculate thrust
+	function thf = thrust()
+
+	end
+
+
+	% compute the torques
+	function tau = torques()
+
+	end
+
+
+	% control function using PD
+	function control = pd_controller()
+
+	end
+
+
+end
