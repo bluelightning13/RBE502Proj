98992621587ba9213a145a53af67ef0e86c997a0
bluelightning13 <mehiatt@wpi.edu>
2016-04-17 14:42:51 -0400

    update
    comments

--- disturbance.m
+++ disturbance.m
@@ -1,8 +1,7 @@
-% wind / disturbance function
-% same F vector with varyine magnitude
-% NOT MOMENT INDUCING
-function [X Y Z] = disturbance() %Don
-    %F = ma;
+%M Hiatt
+%This function generates a random small vector to simulate wind force.
+%This function will be applied within the controller function
+function [X Y Z] = disturbance()
     X = rand([3,8]);
     Y = rand([3,8]);
     Z = 0; %assuming no Z forces for initial integration of functions
