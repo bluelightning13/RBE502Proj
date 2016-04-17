afb04ba4d0f6f5f4060859152e85ef56181d3094
bluelightning13 <mehiatt@wpi.edu>
2016-04-17 14:25:23 -0400

    genrandom
    generates random variables for wind forces

--- disturbance.m
+++ disturbance.m
@@ -1,7 +1,9 @@
 % wind / disturbance function
-	% same F vector with varyine magnitude
-	% NOT MOMENT INDUCING
-	function [X Y Z] = disturbance() %Don
-        %random
-        %F = ma;
-	end
+% same F vector with varyine magnitude
+% NOT MOMENT INDUCING
+function [X Y Z] = disturbance() %Don
+    %F = ma;
+    X = rand([3,8]);
+    Y = rand([3,8]);
+    Z = 0; %assuming no Z forces for initial integration of functions
+end
