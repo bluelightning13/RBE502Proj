1b91d7e8bee489cc9c6d80cb74580f812f6f52f8
bluelightning13 <mehiatt@wpi.edu>
2016-04-20 17:27:57 -0400

    update
    update

--- outline.m
+++ outline.m
@@ -1,35 +1,12 @@
 % main function where everything runs
 function outline()
     %main function 
-    points=[0 1 0; 2 3 0; 1 5 0];
-    pts = spline(points(:,1),points(:,2),points(:,3));
-    %pts2 = interp1();
-    figure('Visible','Off');
+    points=[0 1 1; 2 3 2; 1 5 8];
+    
+    %makes cubic pts
     fnplt(cscvn(points))
     hold on;
-    %plot(points(1,:),points(2,:),'o'), hold off
-    %p2 = 3/8*pi;
-    %p3 = -1/(16*pi^2);
-    %ti = 0;
-    %tf=5*pi; %seconds
-    %dt = 0.016;
-    %t = [ti:dt:tf];
-    %gamma = polyval([p3 p2 0 0], t)
-    
-    
-    
-    %a = 0.0625;
-    %b = 0.25;
-    %x = (b-a)*sin(t) + 3*a*sin((t/a)*(b-a));
-    %z = 0.5+(b-a)*cos(t) - 3*a*cos((t/a)*(b-a));
-    %plot(gamma);
-    
-    
-    %trajectory - cubic poly to/from two points
-    
-    %for every point
-    %call controller
-    %call disturbance
+
     
 	h = findobj(gca,'Type','line');
     x=get(h,'Xdata');
@@ -38,11 +15,7 @@ function outline()
     X = x';
     Y = y';
     Z = z';
-    figure('Visible','On')
     plot3(X(:,1),Y(:,1),Z(:,1));
-	
-	
-    
     % initialize quadcopter state variables
     actual = zeros(1,18);
 	
